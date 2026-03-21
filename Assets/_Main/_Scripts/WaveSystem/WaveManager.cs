46b22c0e22fe469040ee24b3d5f322843e8c0df7
Shad1qq <showdragonx@gmail.com>
2026-03-21 09:18:23 +0300

    update

--- Assets/_Main/_Scripts/WaveSystem/WaveManager.cs
+++ Assets/_Main/_Scripts/WaveSystem/WaveManager.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
+   [Header("Wave Settings")]
    [SerializeField]private double _localStep = 0.00001;
    [SerializeField]private List<WavePattern> _wavePatterns = new List<WavePattern>();
-   [SerializeField]private Vector2 _areaPointOne;
-   [SerializeField]private Vector2 _areaPointTwo;
+   [SerializeField]private Transform[] _areaPoints;
+   
    
    private double _localDifficulty = 1.00;
    private WaveSpawner _waveSpawner;
@@ -27,19 +28,11 @@ public class WaveManager : MonoBehaviour
       WavePattern pattern = _wavePatterns[Random.Range(0, _wavePatterns.Count - 1)];
       CountOfEnemiesInWave = pattern.NumberOfEnemies;
       
-      _waveSpawner.SpawnEnemies(CountOfEnemiesInWave, _areaPointOne, _areaPointTwo);
+      _waveSpawner.SpawnEnemies(CountOfEnemiesInWave, _areaPoints);
    }
    
    private void Update()
    {
       _localDifficulty += _localStep;
    }
-
-   private void OnDrawGizmos()
-   {
-      Gizmos.DrawSphere(_areaPointOne, .1f);
-      Gizmos.DrawSphere(_areaPointTwo, .1f);
-   }
-
-   
 }
--- Assets/_Main/_Scripts/WaveSystem/WaveSpawner.cs
+++ Assets/_Main/_Scripts/WaveSystem/WaveSpawner.cs
@@ -2,16 +2,13 @@
 
 public class WaveSpawner
 {
-    public void SpawnEnemies(int numberOfEnemies, Vector2 areaPointOne, Vector2 areaPointTwo)
+    public void SpawnEnemies(int numberOfEnemies, Transform[] areaPoints)
     {
         for (int i = 0; i < numberOfEnemies; i++)
         {
-            float x = Random.Range(areaPointOne.x, areaPointTwo.x);
-            float y = Random.Range(areaPointOne.y, areaPointTwo.y);
-        
-            Vector2 randomPos = new Vector2(x, y);
-
-            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = randomPos;
+            Transform currentPoint = areaPoints[Random.Range(0, areaPoints.Length-1)];
+            //Random enemy
         }
+        
     }
 }
