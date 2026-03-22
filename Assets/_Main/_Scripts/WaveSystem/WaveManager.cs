1b80db3bd73e8e38cae5faf7848d0882d40ad79d
Shad1qq <showdragonx@gmail.com>
2026-03-22 11:57:52 +0300

    navmesh and wave system

--- Assets/_Main/_Scripts/WaveSystem/WaveManager.cs
+++ Assets/_Main/_Scripts/WaveSystem/WaveManager.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class WaveManager : MonoBehaviour
 {
    [Header("Wave Settings")]
    [SerializeField]private double _localStep = 0.00001;
+   [SerializeField]private int _durationToNextWave = 5;
+   
    [SerializeField]private List<WavePattern> _wavePatterns = new List<WavePattern>();
    [SerializeField]private Transform[] _areaPoints;
-   
    
    private double _localDifficulty = 1.00;
    private WaveSpawner _waveSpawner;
@@ -19,20 +22,27 @@ public class WaveManager : MonoBehaviour
    private void Start()
    {
       _waveSpawner = new WaveSpawner();
-      
-      NextWave();
    }
 
-   private void NextWave()
+   private async UniTask NextWave()
    {
       WavePattern pattern = _wavePatterns[Random.Range(0, _wavePatterns.Count - 1)];
       CountOfEnemiesInWave = pattern.NumberOfEnemies;
+
+      await UniTask.WaitForSeconds(_durationToNextWave);
       
-      _waveSpawner.SpawnEnemies(CountOfEnemiesInWave, _areaPoints);
+      Debug.Log(CountOfEnemiesInWave + " Spawned");
+      
+      _waveSpawner.SpawnEnemies(CountOfEnemiesInWave, _areaPoints, pattern.Enemies);
    }
    
-   private void Update()
+   private async void Update()
    {
       _localDifficulty += _localStep;
+      if (CountOfEnemiesInWave <= 0)
+      { 
+         Debug.Log("Use");
+         await NextWave();
+      }
    }
 }
